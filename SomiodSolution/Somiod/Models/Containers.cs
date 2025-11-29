e943b2c9096bea62aeeaafb077aeb666273a2656
Ricardo Oiteiro <2201766@my.ipleiria.pt>
2025-11-29 12:13:08 +0000

    Corrigir SomiodSolution (deixar de ser submódulo)

---
+++ SomiodSolution/Somiod/Models/Containers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Somiod.Models
+{
+    public class Containers
+    {
+        public string ResType { get; set; } = "container";
+        public string ResourceName { get; set; }
+        public string CreationDatetime { get; set; }
+    }
+}
---
+++ SomiodSolution/Somiod/Models/Subscriptions.cs
@@ -0,0 +1,16 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Somiod.Models
+{
+    public class Subscriptions
+    {
+        public string ResType { get; set; } = "subscription";
+        public string ResourceName { get; set; }
+        public string CreationDatetime { get; set; }
+        public int Evt { get; set; }
+        public string Endpoint { get; set; }
+    }
+}
